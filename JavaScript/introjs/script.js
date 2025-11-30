1eae0ace4ecbb77d3bf45cb0d29becb91c83a327
Adsskk19 <saikiranankam19@gmail.com>
2025-11-30 23:22:36 +0530

    Html and Javascripts basics

---
+++ JavaScript/introjs/script.js
@@ -0,0 +1,29 @@
+//alert("How ar you")
+//const and var difference
+// var a = prompt("Give me  number 1");
+// console.log(a);
+// var b = prompt("Give me  number 2");
+// console.log(b);
+
+// const c = parseInt(a);
+// const d = parseInt(b);
+
+// console.log(c+d);
+
+//const -> block scoped cannot be re-assigned.
+// const a =10;
+// console.log(a)
+// a =12
+// console.log(a)
+
+
+//datatypes
+var f = null;
+console.log(f)
+
+var x = true;
+console.log(x)
+
+var z = false
+console.log(z)
+
