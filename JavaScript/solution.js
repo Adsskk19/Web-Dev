431e4d144ae859ab18bed71a09802556aded41c9
A D S S Kiran Kumar <86604076+Adsskk19@users.noreply.github.com>
2026-01-04 11:09:22 +0530

    Update solution.js

--- JavaScript/solution.js
+++ JavaScript/solution.js
@@ -1,1 +1,10 @@
 //mdn docs for JavaScript
+console.log("hello world");
+console.info("Info message");
+console.warn("Warning msg");
+console.error("Error msg")
+console.confirm(Yes)
+
+// let name = prompt("enter your name:")
+alert("Red Alert");
+let resp = confirm("are you sure!")
